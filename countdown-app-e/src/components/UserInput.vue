175e41ae4b7fb347bd2d959caaa063c99eb9795c
Max Manthey <max@Maxs-MacBook-Pro.local>
2021-02-18 23:54:18 +0000

    day3 commits

---
+++ countdown-app-e/src/components/UserInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-wrapper">
+    <b-input v-model="name"></b-input>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.input-wrapper {
+  padding-top: 2%;
+  width: 30%;
+  margin: 0 auto;
+  overflow: hidden;
+}
+</style>

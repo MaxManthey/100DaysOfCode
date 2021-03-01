11e5227aa0f0b2e32c963ef5948f377da53065d2
Max Manthey <max@Maxs-MacBook-Pro.local>
2021-03-01 02:15:51 +0000

    started new project

---
+++ i-math-quiz-app/src/components/BackButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <b-button><router-link to="/">Back</router-link></b-button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>

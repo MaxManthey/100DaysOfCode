672db64f1262517412676422aed1aa5aa2b58a8e
MaxManthey <m.manthey@axial3d.com>
2021-03-09 18:16:52 +0000

    setup project structure and basic css

--- i-apple-music-frontend-clone/src/views/Home.vue
+++ i-apple-music-frontend-clone/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main-screen />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import MainScreen from '../components/MainScreen.vue'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    MainScreen
   }
 }
 </script>

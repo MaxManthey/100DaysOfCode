647028eb05ca03f99d419a9e99089bcbba46f1d9
MaxManthey <m.manthey@axial3d.com>
2021-03-24 17:49:26 +0000

    finished playlist style

--- i-apple-music-frontend-clone/src/components/UserPlaylist.vue
+++ i-apple-music-frontend-clone/src/components/UserPlaylist.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="wrapper">
-        <img src="https://picsum.photos/250/250" alt="Placeholder image" class="titlepic">
+        <img src="https://picsum.photos/250/250" alt="Placeholder image" class="title-pic">
         <div class="is-size-3 has-text-weight-bold">{{ playlistName }}</div>
         <span>
             <button class="music-btn">Play</button>
@@ -10,11 +10,12 @@
         <ul>
             <li v-for="{song, artist} in getSongs" :key="song">
                 <span class="song-wrapper">
-                    <img src="https://bulma.io/images/placeholders/32x32.png">
-                    <div>
+                    <img src="https://bulma.io/images/placeholders/32x32.png" class="song-pic">
+                    <div class="is-size-7 song-text">
                         <p>{{ song }}</p>
                         <p>{{ artist }}</p>
                     </div>
+                    <p class="test">time</p>
                 </span>
                 <hr class="songs-line">
             </li>
@@ -58,20 +59,42 @@ export default {
 }
 hr.break-line {
     border-top: 1px solid #d4d4d4;
-    width: 90%;
+    width: 77%;
     margin: 3% 5%;
+    margin-left: 11%;
 }
 hr.songs-line {
     border-top: 1px solid #d4d4d4;
-    width: 70%;
-    margin-left: 20%;
+    width: 65%;
+    margin-left: 23%;
     margin-top: 0%
 }
-img.titlepic {
+img {
     border-radius: 5px;
+}
+img.title-pic {
     -webkit-box-shadow: 2px 3px 8px 5px #ccc;  /* Safari 3-4, iOS 4.0.2 - 4.2, Android 2.3+ */
     -moz-box-shadow:    2px 3px 8px 5px #ccc;  /* Firefox 3.5 - 3.6 */
     box-shadow:         2px 3px 8px 5px #ccc; 
+}
+img.song-pic {
+    width: 36px;
+    height: 36px;
+    margin-right: 3%;
+}
+.song-wrapper {
+    display: flex;
+    padding-left: 8%;
+    margin: 3%;
+    margin-bottom: 1%;
+    margin-top: 1%;
+}
+.song-wrapper p:last-child {
+  margin-left: auto;
+  margin-right: 11%;
+}
+.song-text {
+    text-align: left;
 }
 .music-btn {
     width: 100px;
@@ -91,10 +114,5 @@ img.titlepic {
     position: relative;
     top: 1px;
     margin-right: 5px;
-}
-.song-wrapper {
-    display: flex;
-    padding-left: 8%;
-    margin: 3%
 }
 </style>

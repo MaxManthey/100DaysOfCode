8b185fa588aa3e0e5c5d7184bde3d9a32d7a0f97
MaxManthey <m.manthey@axial3d.com>
2021-04-07 16:47:15 +0100

    finished project

--- i-music-lyrics-app/src/store/index.js
+++ i-music-lyrics-app/src/store/index.js
@@ -3,21 +3,27 @@ import Vuex from 'vuex'
 import lyricsFinder from 'lyrics-finder';
 
 Vue.use(Vuex)
+Vue.use(lyricsFinder)
+
 
 export default new Vuex.Store({
   state: {
-    lyrics: ""
+    lyrics: "",
+    song: "",
+    artist: ""
   },
   mutations: {
-    updateLyrics(state, songLyrics) {
-      state.lyrics = songLyrics;
+    updateLyrics(state, payload) {
+      state.lyrics = payload.lyrics;
+      state.song = payload.song;
+      state.artist = payload.artist;
     }
   },
   actions: {
-    async updateLyrics(context, details) {
-      let lyrics = await lyricsFinder("bon jovi", "livin' on a prayer") || "nf";
-      console.log("lyrics: ", lyrics);
-      context.commit("updateLyrics", lyrics)
+    async updateLyrics(context, payload) {
+      let lyrics = await lyricsFinder(payload.artist, payload.song) || "nf";
+      payload.lyrics = lyrics
+      context.commit("updateLyrics", payload)
     }
   },
   modules: {
